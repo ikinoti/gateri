4349f2558577edc79145a60548a07448ad7399b2
ikinoti <gitongakinoti1@gmail.com>
2024-06-14 20:40:44 +0300

    about me model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+# About Model
+class AboutMe(models.Model):
+    short_description = models.TextField()
+    description = models.TextField()
+    image = models.ImageField(upload_to="about")
+
+    class Meta:
+        verbose_name = "About Me"
+        verbose_name_plural = "About Me"
+    
+    def __str__(self):
+        return "About Me"
